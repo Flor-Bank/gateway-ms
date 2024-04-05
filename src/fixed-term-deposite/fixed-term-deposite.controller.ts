3e79a7e38692b0e1881b02b77732bd42e85a215e
florMignini <flormignini29@gmail.com>
2024-04-05 08:35:16 -0300

    f-t-d to client controller connection

--- src/fixed-term-deposite/fixed-term-deposite.controller.ts
+++ src/fixed-term-deposite/fixed-term-deposite.controller.ts
@@ -1,6 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { FIXED_TERM_SERVICE } from 'src/config';
 
 @Controller('fixed-term-deposite')
 export class FixedTermDepositeController {
-  constructor() {}
+  constructor(
+    @Inject(FIXED_TERM_SERVICE)
+    private readonly fixedTermDepositeClient: ClientProxy,
+  ) {}
 }
