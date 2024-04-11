1b1424a0d825e233642bc58d985b415ea4c9d328
florMignini <flormignini29@gmail.com>
2024-04-11 09:06:11 -0300

    user auth guard setted

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,12 +1,21 @@
 import {
   CanActivate,
   ExecutionContext,
+  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Request } from 'express';
+import { ClientProxy } from '@nestjs/microservices/client';
+import { firstValueFrom } from 'rxjs';
+import { NATS_CLIENT } from 'src/config';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(
+    @Inject(NATS_CLIENT)
+    private readonly authClient: ClientProxy,
+  ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
@@ -14,6 +23,12 @@ export class AuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
     try {
+      const { user, token: sessionToken } = await firstValueFrom(
+        this.authClient.send('auth.verify.user', { token }),
+      );
+
+      request['sessionUser'] = user;
+      request['sessionToken'] = sessionToken;
     } catch {
       throw new UnauthorizedException();
     }
