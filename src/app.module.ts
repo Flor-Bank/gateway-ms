bbc449f8f58e987b5522de03bab8440731d900a1
florMignini <flormignini29@gmail.com>
2024-04-17 07:08:21 -0300

    transaction resource creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { FixedTermDepositeModule } from './fixed-term-deposite/fixed-term-deposite.module';
 import { NatsModule } from './transports/nats.module';
 import { AuthModule } from './auth/auth.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [FixedTermDepositeModule, NatsModule, AuthModule],
+  imports: [FixedTermDepositeModule, NatsModule, AuthModule, TransactionModule],
 })
 export class AppModule {}
