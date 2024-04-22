4e3a845bb522e3a3ab7ae7592c6130202c824eaa
florMignini <flormignini29@gmail.com>
2024-04-22 19:14:03 -0300

    get a single transaction by id

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, ParseUUIDPipe, Post } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_CLIENT } from 'src/config';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
@@ -35,7 +35,7 @@ export class TransactionController {
 
   // get single transaction by id
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.transactionClient.send('transaction.findOne', { id }).pipe(
       catchError((error) => {
         throw new RpcException(error);
