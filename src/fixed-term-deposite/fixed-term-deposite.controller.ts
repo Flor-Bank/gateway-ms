6bbb7d3f0a9536c6d911dce1aac5a911542d5d2f
florMignini <flormignini29@gmail.com>
2024-04-28 19:48:16 -0300

    create

--- src/fixed-term-deposite/fixed-term-deposite.controller.ts
+++ src/fixed-term-deposite/fixed-term-deposite.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Inject } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Body, Post, Controller, Inject } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_CLIENT } from 'src/config';
+import { CreateFixedTermDepositeDto } from './dto/create-fixed-term-deposite.dto';
+import { catchError } from 'rxjs';
 
 @Controller('fixed-term-deposite')
 export class FixedTermDepositeController {
@@ -8,4 +10,16 @@ export class FixedTermDepositeController {
     @Inject(NATS_CLIENT)
     private readonly fixedTermDepositeClient: ClientProxy,
   ) {}
+
+  //create a fixed term deposite
+  @Post()
+  create(@Body() createFixedTermDeposite: CreateFixedTermDepositeDto) {
+    return this.fixedTermDepositeClient
+      .send('fixedTermDeposite.create', createFixedTermDeposite)
+      .pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      );
+  }
 }
