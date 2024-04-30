a7a72c401dd205d3d74ad923eda549917691496f
florMignini <flormignini29@gmail.com>
2024-04-30 08:15:53 -0300

    find all controller

--- src/fixed-term-deposite/fixed-term-deposite.controller.ts
+++ src/fixed-term-deposite/fixed-term-deposite.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Post, Controller, Inject } from '@nestjs/common';
+import { Body, Post, Controller, Inject, Get, Query } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { NATS_CLIENT } from 'src/config';
 import { CreateFixedTermDepositeDto } from './dto/create-fixed-term-deposite.dto';
 import { catchError } from 'rxjs';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('fixed-term-deposite')
 export class FixedTermDepositeController {
@@ -16,6 +17,18 @@ export class FixedTermDepositeController {
   create(@Body() createFixedTermDeposite: CreateFixedTermDepositeDto) {
     return this.fixedTermDepositeClient
       .send('fixedTermDeposite.create', createFixedTermDeposite)
+      .pipe(
+        catchError((error) => {
+          throw new RpcException(error);
+        }),
+      );
+  }
+
+  // get all fixed-term-deposite
+  @Get()
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.fixedTermDepositeClient
+      .send('fixedTermDeposite.findAll', { paginationDto })
       .pipe(
         catchError((error) => {
           throw new RpcException(error);
