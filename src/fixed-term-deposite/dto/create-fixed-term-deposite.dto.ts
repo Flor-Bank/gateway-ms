2672ac120e57523f9aded7738a004e9fde85d8ed
florMignini <flormignini29@gmail.com>
2024-05-06 08:02:16 -0300

    update create dto

--- src/fixed-term-deposite/dto/create-fixed-term-deposite.dto.ts
+++ src/fixed-term-deposite/dto/create-fixed-term-deposite.dto.ts
@@ -1,25 +1,45 @@
-import {
-  IsDateString,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-} from 'class-validator';
+import { IsDateString, IsEnum, IsNumber, IsPositive } from 'class-validator';
 import {
   fixedTermDepositeStatus,
   FixedTermDepositeStatusList,
 } from '../enum/fixed-term-deposite.enum';
+import { Type } from 'class-transformer';
 
 export class CreateFixedTermDepositeDto {
-  @IsNumber()
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
   @IsPositive()
-  amount: number;
+  @Type(() => Number)
+  amount_to_invest: number;
   @IsEnum(FixedTermDepositeStatusList, {
     message: `Allowed status values: ${FixedTermDepositeStatusList}`,
   })
-  @IsOptional()
-  status: fixedTermDepositeStatus = fixedTermDepositeStatus.ACTIVE;
-  @IsOptional()
-  @IsDateString() //?or @IsDate()
-  termEndDate: Date;
+  status: fixedTermDepositeStatus;
+
+  @IsDateString()
+  @Type(() => Date)
+  maturity_date: Date;
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @IsPositive()
+  @Type(() => Number)
+  interest_rate: number;
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @IsPositive()
+  @Type(() => Number)
+  interest_rate_amount: number;
+
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @IsPositive()
+  @Type(() => Number)
+  total_amount: number;
+  @IsDateString()
+  @Type(() => Date)
+  creation_date: Date;
 }
