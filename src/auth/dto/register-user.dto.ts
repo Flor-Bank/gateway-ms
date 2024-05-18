8431d566110723bb47cfe2665d8a1a4fc8e3b4e4
florMignini <flormignini29@gmail.com>
2024-05-18 08:27:14 -0300

    dto update

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -1,15 +1,32 @@
-import { IsNumber, IsString, IsStrongPassword } from 'class-validator';
+import {
+  IsBoolean,
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterUserDto {
-  @IsNumber()
-  identityNumber: number;
   @IsString()
-  firstName: string;
-  @IsString()
-  lastName: string;
-  @IsString()
+  @MinLength(6)
   username: string;
   @IsString()
-  @IsStrongPassword()
+  @MinLength(1)
+  firstName: string;
+  @IsString()
+  @MinLength(1)
+  lastName: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
+  })
   password: string;
+  @IsBoolean()
+  isActive: boolean = true;
 }
